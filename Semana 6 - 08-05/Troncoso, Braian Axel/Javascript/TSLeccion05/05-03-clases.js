fd391b72c8e3216d887faaefb4187c61a5b5d7b7
BraianTroncoso <95662710+BraianTroncoso@users.noreply.github.com>
2023-05-24 17:31:26 -0300

    [REFACTOR] archivos mal subidos

---
+++ Semana 6 - 08-05/Troncoso, Braian Axel/Javascript/TSLeccion05/05-03-clases.js
@@ -0,0 +1,52 @@
+//let persona3 = new Persona('Prueba','Pruebita'); // No se puede inicizalizar una clase(no aplica el hosting como en las funciones que si se pueden llamar y después declararse)
+class Persona{
+    constructor(nombre, apellido){ // Sino se define el constructor js añade uno vacio de forma automatica
+        this._nombre = nombre;
+        this._apellido = apellido;
+    }
+    get nombre(){
+        return this._nombre;
+    }
+    get apellido(){
+        return this._apellido;
+    }
+
+    set nombre(nombre){
+        this._nombre = nombre;
+    }
+    set apellido(apellido){
+        this._apellido = apellido;
+    }
+}
+
+class Empleado extends Persona{
+    constructor(nombre, apellido, departamento){
+        super(nombre, apellido);
+        this._departamento = departamento;
+    }
+
+    get departamento(){
+        return this.departamento;
+    }
+    set departamento(departamento){
+        return this.departamento = this.departamento;
+    }
+}
+let persona1 = new Persona ('Martin','Perez');
+console.log(persona1.nombre);
+console.log(persona1.apellido);
+persona1.nombre = 'Juan Carlos';
+persona1.apellido = 'Troncoso';
+console.log(persona1.nombre +' '+persona1.apellido);
+
+let persona2 = new Persona('Carlos','Lara');
+persona2.nombre = 'Maria Laura'
+console.log(persona2.nombre);
+console.log(persona2.apellido);
+persona2.nombre = 'Axel';
+persona2.nombre = 'Troncoso';
+console.log(persona2.nombre+' '+persona2.apellido)
+
+let empleado1 = new Empleado('Maria','Gimenez','Sistema');
+console.log(empleado1);
+console.log(empleado1.nombre);
