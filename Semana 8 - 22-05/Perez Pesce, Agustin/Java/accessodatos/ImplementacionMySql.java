9cdd1252105bbc1beef1ee251784d3e9a79cec5f
Agustín Pérez Pesce <aguppesce@gmail.com>
2023-05-25 13:53:30 -0600

    [ADD] Ejercicios Java, JavaScript, Python Semana 8

---
+++ Semana 8 - 22-05/Perez Pesce, Agustin/Java/accessodatos/ImplementacionMySql.java
@@ -0,0 +1,25 @@
+// ===============CLASE 07 - MIERCOLES-24/05/2023===============
+package accessodatos;
+
+public class ImplementacionMySql implements IAccesoDatos{
+
+    @Override
+    public void insertar() {
+        System.out.println("Insertar desde MySql");
+    }
+
+    @Override
+    public void listar() {
+        System.out.println("Listar desde MySql");
+    }
+
+    @Override
+    public void actualizar() {
+        System.out.println("Actualizar desde MySql");
+    }
+
+    @Override
+    public void eliminar() {
+        System.out.println("Eliminar desde MySql");
+    }
+}
