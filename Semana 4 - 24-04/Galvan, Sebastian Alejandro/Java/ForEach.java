818b647e3c0dec8ba5bfee6dfb432127562a989e
Sebastián Galván <sebastiangalvan.ar@gmail.com>
2023-05-27 15:13:46 -0300

    Semana 4 Seba

---
+++ Semana 4 - 24-04/Galvan, Sebastian Alejandro/Java/ForEach.java
@@ -0,0 +1,23 @@
+package Clase3;
+
+public class ForEach {
+    public static void main(String[] args) {
+        int edades[] = {5, 6, 8, 9};
+
+        for (int edad: edades
+             ) {
+            System.out.println("edad = " + edad);
+        }
+
+
+        Persona personas[] =  {new Persona("Juan"),
+        new Persona("Maria"), new Persona("Luis")};
+
+        for (Persona persona: personas
+             ) {
+            System.out.println("persona = " + persona);
+        }
+    }
+}
+
+//Video 2.4
