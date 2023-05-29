3bb8e05b211a1993fdb69bf86ba222ee2fc4af50
BraianTroncoso <95662710+BraianTroncoso@users.noreply.github.com>
2023-05-28 22:06:55 -0300

    [FIX] semana 8

--- Semana 1 - 03-04/Farias, Mariano/Python/NumerosIgualesExeption.py
+++
@@ -1,3 +0,0 @@
-class NumerosIgualesExeption (Exception): # Extiende de la clase
-    def __init__(self, mensaje):
-        self.message = mensaje
--- Semana 3 - 17-04/Mariano Farias/Javascrip/01-03-funciones.js
+++
@@ -1,7 +0,0 @@
-miFuncion(8, 2); //Esto se lo conoce como hosting
-function miFuncion(a, b){
-    console.log("Sumamos: "+(a +b));
-}
-
-//LLamando la funcion 
-miFuncion(5, 4);
