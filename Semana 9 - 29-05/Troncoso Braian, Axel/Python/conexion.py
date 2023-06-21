0c76d9d00212d9138cb9cdf2c1c02127553bcd4b
BraianTroncoso <95662710+BraianTroncoso@users.noreply.github.com>
2023-06-21 15:13:31 -0300

    [ADD] Archivos de Python - Js

---
+++ Semana 9 - 29-05/Troncoso Braian, Axel/Python/conexion.py
@@ -0,0 +1,49 @@
+import sys
+
+from psycopg2 import pool
+from logger_base import log
+class Conexion:
+    _DATABASE='test_bd'
+    _USERNAME='postgres'
+    _PASSWORD='postgres'
+    _DB_PORT='5432'
+    _HOST='127.0.0.1'
+    _MIN_CON = 1
+    _MAX_CON = 5
+    _pool = None
+
+    @classmethod
+    def obtenerConexion(cls):
+       conexion = cls.obtenerPool().getconn()
+       log.debug(f'Conexion obtenida del pool: {conexion}')
+       return conexion
+
+    @classmethod
+    def obtenerCursor(cls):
+       pass
+
+    @classmethod
+    def obtenerPool(cls):
+        if cls._pool is None:
+            try:
+                cls._pool = pool.SimpleConnectionPool(cls._MIN_CON,
+                                                      cls._MAX_CON,
+                                                      host=cls._HOST,
+                                                      user=cls._USERNAME,
+                                                      password=cls._PASSWORD,
+                                                      port=cls._DB_PORT,
+                                                      database=cls._DATABASE)
+                log.debug(f'Creacion del pool exitosa: {cls._pool}')
+                return  cls._pool
+            except Exception as e:
+                log.error(f'Ocurrió un error al obtener el pool{e}')
+                sys.exit()
+        else:
+            return cls._pool
+if __name__ == '__main__':
+    conexion1 = Conexion.obtenerConexion()
+    conexion2 = Conexion.obtenerConexion()
+    conexion3 = Conexion.obtenerConexion()
+    conexion4 = Conexion.obtenerConexion()
+    conexion5 = Conexion.obtenerConexion()
+    #conexion6 = Conexion.obtenerConexion() # Saltaria error porque el maximo de conexiones a la vez son 5(caso de que el max sea 5)
