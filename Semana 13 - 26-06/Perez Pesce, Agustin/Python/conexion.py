c72e1c5c5a436c2cc811cc3e2d2b4168189fe22e
Agustín Pérez Pesce <aguppesce@gmail.com>
2023-07-04 22:25:52 -0300

    [ADD] Ejercicio Java y Python Semana 13

---
+++ Semana 13 - 26-06/Perez Pesce, Agustin/Python/conexion.py
@@ -0,0 +1,65 @@
+# ===============CLASE 08 - LUNES-29/05/2023===============
+
+from psycopg2 import pool
+# psycopg2 as db # esta es otra forma de importar el psycopg2
+from logger_base import log
+import sys
+
+class Conexion:
+    _DATABASE = 'test_bd'
+    _USERNAME = 'postgres'
+    _PASSWORD = 'postgres'
+    _DB_PORT = '5432'
+    _HOST = '127.0.0.1'
+    _MIN_CON = 1
+    _MAX_CON = 5
+    _pool = None
+
+# ===============CLASE 10 - LUNES-12/06/2023===============
+    @classmethod
+    def obtenerConexion(cls):
+        conexion = cls.obtenerPool().getconn()
+        log.debug(f'Conexión obtenida del pool: {conexion}')
+        return conexion
+
+    @classmethod
+    def obtenerPool(cls):
+        if cls._pool is None:
+            try:
+                cls._pool = pool.SimpleConnectionPool(cls._MIN_CON,
+                                                      cls._MAX_CON,
+                                                      host=cls._HOST,
+                                                      user=cls._USERNAME,
+                                                      password=cls._PASSWORD,
+                                                      port=cls._DB_PORT,
+                                                      database=cls._DATABASE)
+                log.debug(f'Creación del pool exitosa: {cls._pool}')
+                return cls._pool
+            except Exception as e:
+                log.error(f'Ocurrió un error al obtener el pool: {e}')
+                sys.exit()
+        else:
+            return cls._pool
+
+    # ===============CLASE 11 - LUNES-26/06/2023===============
+    @classmethod
+    def liberarConexion(cls, conexion):
+        cls.obtenerPool().putconn(conexion)
+        log.debug(f'Regresamos la conexión del pool: {conexion}')
+
+    @classmethod
+    def cerrarConexiones(cls):
+        cls.obtenerPool().closeall()
+
+if __name__ == '__main__':
+    conexion1 = Conexion.obtenerConexion()
+    Conexion.liberarConexion(conexion1)
+    conexion2 = Conexion.obtenerConexion()
+    Conexion.liberarConexion(conexion2)
+    conexion3 = Conexion.obtenerConexion()
+    Conexion.liberarConexion(conexion3)
+    conexion4 = Conexion.obtenerConexion()
+    conexion5 = Conexion.obtenerConexion()
+    conexion6 = Conexion.obtenerConexion()
+
+
